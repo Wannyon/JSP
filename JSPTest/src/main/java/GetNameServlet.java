c3f3594f4730d979fc1efeddc4e65ee820e6eb13
Wannyon <ghkdwodnjs123@naver.com>
2023-03-13 18:11:20 +0900

    2023-03-13
    JSP 구성 파일
    - webapp: 웹과 관련된 소스파일 배치(html,jsp 등...)
    - WEB-INF: web.xml과 lib폴더를 배치
    - web.xml: 해당 프로젝트의 web과 관련된 설정파일
    - lib: 각종 외부 라이브러리를 배치
    
    - java: 자바 소스파일을 배치
    
    지시어 문법: <%@...%>
    표현식 문법: <%=...%>
    스크립틀릿(자바 코드 기술): <%...%>
    
    CurrentTime.jsp => CurrentTime_jsp.java => CurrentTime_jsp.class => HTML코드 생성
    
    아파치 톰캣에서 자동 생성하는 내장객체(변경 불가)
    C:\Project\JSP_Source\.metadata\.plugins\org.eclipse.wst.server.core
    \tmp0\work\Catalina\localhost\JSPTest\org\apache\jsp
    - request*: 요청과 관련된 정보(요청 객체)
    - response*: 응답과 관련된 정보(응답 객체)
    - pageContext
    - session*
    - application*
    - config
    - out
    - page
    
    HTTP : Hyper Text Transfer Prtocol
    
    *** JSP에서 한글처리 ***
    1. server.xml에서 아래 형식으로 수정(URLEncoding=utf-8를 추가함)
    <Connector connectionTimeout=20000 port=8800 protocol=HTTP/1.1 redirectPort=8443 URIEncoding=utf-8/>
    
    2. html 또는 jsp 문서에서 charset=utf-8로 설정
    
    3. Servlet 클래스에서 doGet 또는 doPost에서 아래와 같이 설정
    request.setCharacterEncoding(utf-8);	// 요청받은 데이터를 읽을 때 사용할 encoding 형식
    response.setContentType(text/html

---
+++ JSPTest/src/main/java/GetNameServlet.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class GetNameServlet
+ */
+@WebServlet("/getName")
+public class GetNameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetNameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();	// 이미 만들어져 있는 세션 객체를 가져옴.
+		String name=(String)session.getAttribute("name");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>name="+name+"</h1>");
+	}
+
+}
